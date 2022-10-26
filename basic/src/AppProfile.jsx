7d14c8a565e0024f235c80a3cc83e45892cae583
credevo <62835973+credevo@users.noreply.github.com>
2022-10-27 06:36:01 +0900

    profile component props defined

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -5,7 +5,9 @@ import Profile from './components/Profile';
 const AppProfile = ()=> {
   return (
    <>
-    <Profile />
+    <Profile imgUrl={'./ironman.jpeg'} name={'아이언맨'} job={'천재 엔지니어'}/>
+    <Profile imgUrl={'./captine.jpeg'} name={'캡틴 아메리카'} job={'솔져'}/>
+    <Profile imgUrl={'./hulk.jpeg'} name={'헐크'} job={'소년 장사'}/>
    </>
 
   );
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,12 +1,11 @@
 import React from 'react'; 
 
-export default function Profile() {
+export default function Profile(props) {
     return(
         <div className="profile">
-            <img class="photo" src="/profileImage.jpeg" alt="avatar"/>
-            <h1> ION MAN</h1>
-            <h2> 프런트 개발자</h2>
-        </div>
+            <img class="photo" src={props.imgUrl} alt="avatar"/>
+            <h1> {props.name}</h1>
+            <h2> {props.job}</h2>
+      </div>
     );
 }
-
