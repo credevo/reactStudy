0ecae45cab0df05205c1adc83504bef5d2530a96
credevo <62835973+credevo@users.noreply.github.com>
2022-11-01 00:11:50 +0900

    folder init

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-// import AppProfile from './AppProfile';
-// import AppCounter from './AppCounter';
-import AppProducts from './AppProducts';
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    {/* <AppProfile /> */}
-    {/* <AppCounter /> */}
-    <AppProducts />
+    <App />
   </>
 );
-
-// If you want to start measuring performance in your AppProfile, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
