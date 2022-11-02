5788ec68abda716514f41061651815d0933077a5
credevo <62835973+credevo@users.noreply.github.com>
2022-11-02 23:29:02 +0900

    appmentos created

---
+++ basic/src/components/AppMento.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppMento(props) {
+  return (
+    <>
+      <li>{`${props.name} 는 ${props.title} 이시다`}</li>
+    </>
+  );
+}
--- basic/src/index.js
+++ basic/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // import AppXy from "./AppXy";
-import AppMento from "./AppMento";
+import AppMentos from "./AppMentos";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     {/* <AppXy /> */}
-    <AppMento />
+    <AppMentos />
   </>
 );
